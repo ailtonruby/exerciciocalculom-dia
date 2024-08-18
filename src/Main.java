0ffbd4439b2344d0bbfdd1bff3456c5f5a735c77
ailtonruby <ailtonruby97@gmail.com>
2024-08-18 10:32:20 -0300

    exercicio

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        calculoM();
+    }
+
+    public static void calculoM(){
+        float num1 = 4.0f;
+        float num2 = 4.0f;
+        float num3 = 4.0f;
+        float num4 = 4.0f;
+        float resultado;
+        resultado = (num1 + num2 + num3 + num4)/4;
+        System.out.println(resultado);
+    }
+}
+
